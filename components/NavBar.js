f980047a799f31cb603ed03a4b199f8933a9ada3
unknown <xueyublue@gmail.com>
2021-09-19 10:07:03 +0800

    modified side nav

--- components/NavBar.js
+++ components/NavBar.js
@@ -9,16 +9,23 @@ import {
 
 import { useState } from "react";
 
+const { SubMenu } = Menu;
 const { Sider } = Layout;
 
 const NavBar = () => {
   const [collapsed, setCollapsed] = useState(false);
 
   return (
-    <Sider trigger={null} collapsible collapsed={collapsed}>
+    <Sider trigger={null} collapsible collapsed={collapsed} style={{
+      overflow: 'auto',
+      height: '100vh',
+      position: 'fixed',
+      left: 0,
+    }}>
       <div className="logo" />
       <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-        <Menu.Item key="1" icon={<UserOutlined />}>
+      <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+      <Menu.Item key="1" icon={<UserOutlined />}>
           nav 1
         </Menu.Item>
         <Menu.Item key="2" icon={<VideoCameraOutlined />}>
@@ -27,6 +34,29 @@ const NavBar = () => {
         <Menu.Item key="3" icon={<UploadOutlined />}>
           nav 3
         </Menu.Item>
+         </SubMenu>
+         <SubMenu key="sub2" icon={<UserOutlined />} title="subnav 1">
+      <Menu.Item key="1" icon={<UserOutlined />}>
+          nav 1
+        </Menu.Item>
+        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+          nav 2
+        </Menu.Item>
+        <Menu.Item key="3" icon={<UploadOutlined />}>
+          nav 3
+        </Menu.Item>
+         </SubMenu>
+         <SubMenu key="sub3" icon={<UserOutlined />} title="subnav 1">
+      <Menu.Item key="1" icon={<UserOutlined />}>
+          nav 1
+        </Menu.Item>
+        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+          nav 2
+        </Menu.Item>
+        <Menu.Item key="3" icon={<UploadOutlined />}>
+          nav 3
+        </Menu.Item>
+         </SubMenu>
       </Menu>
     </Sider>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,42 +1,32 @@
+import { Layout } from "antd";
+
 import NavBar from "../components/NavBar";
 import "../styles/globals.css";
-import React from "react";
 import "antd/dist/antd.css";
 import "./index.css";
-import { Layout } from "antd";
-import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
 
-const { Header, Content } = Layout;
-
-const toggle = () => {
-  // this.setState({
-  //   collapsed: !this.state.collapsed,
-  // });
-  console.log("toggle clicked.");
-};
+const { Header, Content, Footer } = Layout;
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout
-      style={{
-        minHeight: "100vh",
-      }}
     >
       <NavBar />
-      <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }}>
+      <Layout className="site-layout" className="site-layout" style={{ marginLeft: 200, minHeight: "100vh" }}>
+        <Header className="site-layout-background" style={{ paddingLeft: 16 }}>
           App bar
         </Header>
         <Content
           className="site-layout-background"
           style={{
             margin: "8px 8px",
-            padding: 24,
+            padding: 8,
             minHeight: 650,
           }}
         >
           <Component {...pageProps} />
         </Content>
+        <Footer style={{ textAlign: 'center' }}>All rights reserved by Daifuku Singapore 2021</Footer>
       </Layout>
     </Layout>
   );
