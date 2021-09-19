a4f2358cdcbe7b3dfd61fd929402fdcca0ee9339
unknown <xueyublue@gmail.com>
2021-09-19 10:52:34 +0800

    added pages

---
+++ pages/instance/details.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InstanceDetails = () => {
+  return <div>Instance Details</div>;
+};
+
+export default InstanceDetails;
---
+++ pages/instance/sga.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SgaConfigurations = () => {
+  return <div>SGA Configurations</div>;
+};
+
+export default SgaConfigurations;
