57794104106b81888bb4910ec076ec2f52049f8d
unknown <xueyublue@gmail.com>
2021-09-22 22:39:06 +0800

    completed instance details

--- pages/instance/details.js
+++ pages/instance/details.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { Table } from "antd";
 
-const InstanceDetails = () => {
-  return <div>Instance Details</div>;
+const columns = [
+  {
+    title: "Field",
+    dataIndex: "name",
+    key: "key",
+  },
+  {
+    title: "Value",
+    dataIndex: "value",
+    key: "key",
+  },
+];
+
+const InstanceDetails = ({ data }) => {
+  console.log(data);
+  return (
+    <div>
+      <Table
+        columns={columns}
+        dataSource={data}
+        bordered
+        size="small"
+        pagination={{ pageSize: 30, position: ["topLeft"] }}
+        title={() => <h3>Instance Details</h3>}
+      />
+    </div>
+  );
 };
 
 export default InstanceDetails;
+
+export async function getServerSideProps(context) {
+  const response = await fetch("http://10.33.1.168:8099/wse/restapi/oms/instance/details");
+  const data = await response.json();
+  return {
+    props: { data: data },
+  };
+}
--- pages/instance/sga.js
+++ pages/instance/sga.js
@@ -1,7 +1,41 @@
 import React from "react";
 
-const SgaConfigurations = () => {
-  return <div>SGA Configurations</div>;
+import { Table } from "antd";
+
+const columns = [
+  {
+    title: "Field",
+    dataIndex: "name",
+    key: "key",
+  },
+  {
+    title: "Value",
+    dataIndex: "value",
+    key: "key",
+  },
+];
+
+const SgaConfigurations = ({ data }) => {
+  return (
+    <div>
+      <Table
+        columns={columns}
+        dataSource={data}
+        bordered
+        size="small"
+        pagination={{ pageSize: 30, position: ["topLeft"] }}
+        title={() => <h3>SGA Configuration</h3>}
+      />
+    </div>
+  );
 };
 
 export default SgaConfigurations;
+
+export async function getServerSideProps(context) {
+  const response = await fetch("http://10.33.1.168:8099/wse/restapi/oms/instance/details");
+  const data = await response.json();
+  return {
+    props: { data: data },
+  };
+}
