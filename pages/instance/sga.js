4b64f5225a63144556a08d3612d31dc2992651c9
unknown <xueyublue@gmail.com>
2021-10-01 14:06:28 +0800

    almost done

--- pages/instance/sga.js
+++ pages/instance/sga.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import { Table, message, Progress } from "antd";
 import { formatNumberWithCommas } from "../../util/util";
+import { Pie } from "react-chartjs-2";
 
 const columns = [
   {
     title: "Name",
     dataIndex: "name",
     key: "name",
-    width: 400,
+    width: 300,
   },
   {
     title: "Size",
@@ -42,14 +43,38 @@ const SgaConfigurations = ({ data }) => {
 
   return (
     <div>
-      <Table
-        title={() => <h3>SGA Configuration</h3>}
-        columns={columns}
-        dataSource={data.table}
-        bordered
-        size="small"
-        pagination={{ pageSize: 15, position: ["none"] }}
-      />
+      <div style={{ width: 800 }}>
+        <Table
+          columns={columns}
+          dataSource={data.table}
+          bordered
+          size="small"
+          pagination={{ pageSize: 15, position: ["none"] }}
+        />
+      </div>
+      <div style={{ height: "500" }}>
+        <Pie
+          data={{
+            labels: data.chart.name,
+            datasets: [
+              {
+                data: data.chart.data,
+                backgroundColor: data.chart.backgroundColor,
+              },
+            ],
+          }}
+          options={{
+            title: { display: false, text: "SGA Configuration (" + data.maxSgaSize + "MB In Total)" },
+            maintainAspectRatio: false,
+            scales: {
+              yAxes: [{ ticks: { display: false }, gridLines: { display: false } }],
+            },
+            legend: { position: "right" },
+          }}
+          height={300}
+          width={600}
+        />
+      </div>
     </div>
   );
 };
