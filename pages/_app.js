3c2c385e4239b8c9fecb89338fe0e03e59ded18e
unknown <xueyublue@gmail.com>
2021-10-01 22:01:22 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,6 @@ function MyApp({ Component, pageProps }) {
   const { asPath } = useRouter();
   if (asPath === "/login" && !pageWithoutNavigation) setPageWithoutNavigation(true);
   if (asPath !== "/login" && pageWithoutNavigation) setPageWithoutNavigation(false);
-  console.log(pageWithoutNavigation);
 
   if (pageWithoutNavigation)
     return (
