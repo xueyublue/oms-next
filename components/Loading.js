7863cb36e46568cbf45b20cb3c3908182fb66788
unknown <xueyublue@gmail.com>
2021-10-18 09:41:28 +0800

    center loading indicator

--- components/Loading.js
+++ components/Loading.js
@@ -2,8 +2,8 @@ import { Spin } from "antd";
 
 const Loading = () => {
   return (
-    <div style={{ textAlign: "center", height: "100%" }}>
-      <Spin size="large" />
+    <div style={{ position: "absolute", left: "50%", top: "50%", transform: "translate(-50%, -50%)" }}>
+      <Spin size="large" tip="Loading..." />
     </div>
   );
 };
